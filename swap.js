261c0af12557e35726dab1e22f0543915cf43034
Harsh Pratap <49613030+harshxjh@users.noreply.github.com>
2022-01-28 13:08:47 +0530

    Add files via upload

---
+++ swap.js
@@ -0,0 +1,14 @@
+
+let a = prompt('Enter the first variable: ');
+let b = prompt('Enter the second variable: ');
+
+
+let c;
+
+//swap variables
+c = a;
+a = b;
+b = c;
+
+console.log(' the value of a ' +  a);
+console.log(' the value of b ' +  b);
