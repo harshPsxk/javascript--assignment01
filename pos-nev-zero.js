2664d9657fcc1ec9ff0a0bf339ceb23f6dbd48cd
Harsh Pratap <49613030+harshxjh@users.noreply.github.com>
2022-01-28 13:15:51 +0530

    Add files via upload

---
+++ pos-nev-zero.js
@@ -0,0 +1,12 @@
+let a = prompt('Enter the first variable: ');
+
+if(a > 0){
+  console.log('the number is positive');
+}
+else if(a < 0){
+  console.log('the number is negative');
+
+}
+else{
+  console.log('the number is zero');
+}
